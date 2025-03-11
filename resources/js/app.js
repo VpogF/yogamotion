c18466c9ba706692b682e8a8ff7dbf6144d29a63
VpogF <vpoglonig2324@politecnics.barcelona>
2025-03-11 18:09:34 +0100

    instalacio
    instalar vue bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+import * as bootstrap from 'bootstrap';
+
+import { createApp } from 'vue'
+
+import prueba from './components/Prueba.vue';
+
+const app = createApp({});
+
+app.component('prueba', prueba);
+
+app.mount('#app');
+
