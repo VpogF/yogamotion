7e15b2b57088f785be0b2a58f58659fa325ad7d6
VpogF <vpoglonig2324@politecnics.barcelona>
2025-03-17 15:45:58 +0100

    api
    trabajando en la creación de apis entorno servidor

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\EventoController;
 use App\Http\Controllers\Api\TipoUsuarioController;
 use App\Http\Controllers\Api\UsuarioController;
 use Illuminate\Http\Request;
@@ -23,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('usuario', UsuarioController::class);
 Route::apiResource('tipousuario', TipoUsuarioController::class);
-Route::apiResource('usuario', UsuarioController::class);
+Route::apiResource('evento', EventoController::class);
 Route::apiResource('usuario', UsuarioController::class);
 Route::apiResource('usuario', UsuarioController::class);
 Route::apiResource('usuario', UsuarioController::class);
