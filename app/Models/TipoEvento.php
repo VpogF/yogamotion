4f5a7a6e630a5db443b3c7112e2b53dcf4f18f6e
VpogF <vpoglonig2324@politecnics.barcelona>
2025-03-17 15:51:45 +0100

    api
    trabajando en la creación de apis entorno servidor

---
+++ app/Models/TipoEvento.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoEvento extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tipo_evento';
+    protected $primaryKey = 'id';
+    public $incrementing = 'true';
+    public $timestamps = 'false';
+
+
+    /**
+     * Get all of the comments for the TipoEvento
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function evento()
+    {
+        return $this->hasMany(Evento::class, 'tipo_evento_id');
+    }
+}
